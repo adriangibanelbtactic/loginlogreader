8c75012029f3d3da0bf8e032b839a457c76195e8
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-03-08 10:14:21 +0100

    Application.php: Remove zextras legal notice.

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -15,7 +15,6 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * If you require any further information, feel free to contact legal@zextras.com.
  */
 
 namespace OCA\LoginLogReader\AppInfo;
