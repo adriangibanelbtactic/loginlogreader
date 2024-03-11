912dc8164f8e20bc9607b5f8551aa116a5e38d48
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-03-11 11:24:52 +0100

    Initial app commit.

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Login Log Reader
+ * Copyright (C) 2024 BTACTIC, S.C.C.L.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\LoginLogReader\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class Application extends App implements IBootstrap
+{
+    const APP_ID = 'loginlogreader';
+
+    public function __construct(array $urlParams=[]){
+        parent::__construct(self::APP_ID, $urlParams);
+    }
+
+    public function register(IRegistrationContext $context): void {
+    }
+
+    public function boot(IBootContext $context): void {
+    }
+
+}
---
+++ templates/index.php
@@ -0,0 +1,75 @@
+<?php
+// TODO: Use variable for actual Nextcloud data and not 'data' literally
+
+function print_login_header () {
+?>
+<table style="width:100%">
+  <tr>
+    <th><b>Time</b></th>
+    <th><b>User</b></th>
+    <th><b>Message</b></th>
+    <th><b>IP</b></th>
+  </tr>
+<?php
+}
+
+function print_login_footer () {
+?>
+</table>
+<?php
+}
+
+function print_login_row ($jsonData) {
+?>
+  <tr>
+    <td><?php echo ($jsonData["time"]); ?></td>
+    <td><?php echo ($jsonData["user"]); ?></td>
+    <td><?php echo ($jsonData["message"]); ?></td>
+    <td><?php echo ($jsonData["remoteAddr"]); ?></td>
+  </tr>
+<?php
+}
+
+function valid_login_message ($jsonData) {
+
+    return ( (str_starts_with($jsonData["message"], "Login successful")) or (str_starts_with($jsonData["message"], "Logout")) );
+
+}
+
+function get_all_lines($audit_log_file_handle) {
+    while (!feof($audit_log_file_handle)) {
+        yield fgets($audit_log_file_handle);
+    }
+}
+
+$audit_log_filename = $_['logfile'];
+$admin_audit_is_installed = $_['adminauditisinstalled'];
+
+if (!($admin_audit_is_installed)) {
+
+  echo ("Make sure that admin_audit is both installed and active.");
+
+} else if (!(file_exists($audit_log_filename))) {
+
+  echo ("File: '". $audit_log_filename . "' does not exist !!! ");
+
+} else {
+
+    $audit_log_file_handle = fopen($audit_log_filename, 'r');
+
+    print_login_header();
+
+    foreach (get_all_lines($audit_log_file_handle) as $line) {
+        $jsonString = $line;
+        $jsonData = json_decode($jsonString, true);
+        if (valid_login_message($jsonData)) {
+            print_login_row($jsonData);
+        }
+    }
+    print_login_footer();
+
+    fclose($audit_log_file_handle);
+
+}
+
+?>
